03f0d51b1bf3b17511b371506f8e7c1c486e67a6
Ubuntu <ubuntu@ip-172-31-5-3.ca-central-1.compute.internal>
2021-12-12 21:45:55 +0000

    modified app.js to say my name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Danielle Slusser
         </p>
         <a
           className="App-link"
